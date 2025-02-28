0cf2fadd0c5ef679e90c816a4bdab03d16d6f208
jaehwannnnnn <seanjung0915@gmail.com>
2025-02-28 17:34:29 +0900

    add file

---
+++ src/main/java/mission/Mission1.java
@@ -0,0 +1,15 @@
+package mission;
+
+import java.util.Scanner;
+
+public class Mission1 {
+    public static void main(String[] args) {
+        System.out.println("이름을 입력하세요.");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("나이를 입력하세요. ");
+        int age = sc.nextInt();
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + age);
+    }
+}
