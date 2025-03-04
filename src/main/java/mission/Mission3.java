1a2159787215a9f8662546d3ce1eb81adf2059f3
jaehwannnnnn <seanjung0915@gmail.com>
2025-03-04 14:25:11 +0900

    Mission3

---
+++ src/main/java/mission/Mission3.java
@@ -0,0 +1,13 @@
+package mission;
+
+import java.util.Scanner;
+
+public class Mission3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("두개의 숫자를 입력하세요.");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("더 큰 숫자 : " + Math.max(num1, num2));
+    }
+}
