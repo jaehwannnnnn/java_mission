fe2be631af29142c8ab7983a56bbf41cb750acb4
jaehwannnnnn <seanjung0915@gmail.com>
2025-03-05 14:34:27 +0900

    Mission5

---
+++ src/main/java/mission/Mission5.java
@@ -0,0 +1,11 @@
+package mission;
+
+public class Mission5 {
+    public static void main(String[] args) {
+        int[] iarr = new int[10];
+        for (int i = 0; i < iarr.length; i++) {
+            iarr[i] = i + 1;
+            System.out.println(iarr[i]);
+        }
+    }
+}
